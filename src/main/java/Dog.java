81d756e3d3574c2879e57c70ca516c2cd963c240
camshaft54 <cam.shaw1234@gmail.com>
2020-10-29 10:00:53 -0700

    Complete Dog.java

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,38 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog() {
+        this.breed = "Shih Tzu";
+    }
+
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
 
     // makeNoise() method
 
+    @Override
+    public void makeNoise() {
+        System.out.println("woof.");
+    }
+
 
     // toString method
-
+    @Override
+    public String toString() {
+        return "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
+    }
 
     // Getter
-
+    public String getBreed() {
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,11 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog d1 = new Dog();
+        System.out.println(d1.toString());
+        Dog d2 = new Dog("Maximum", 9001, "Great Dane");
+        System.out.println(d2.toString());
+        d2.setBreed("Dachshund");
+        System.out.println(d2.getBreed());
+        d2.makeNoise();
     }
 }
